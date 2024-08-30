97c29f71ea1979382405946cd88a3508af9747ff
Khariza <kharizaazmi@gmail.com>
2024-08-30 09:37:50 +0700

    modul test odoo14

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name" : "Modul order work",
+    "depends" : ["base", "account", "sale"],
+    "author" : "admin",
+    "category" : "ERP",
+    "website" : "http://xxxx",
+    "description" : "Modul pemesanan work service",
+    "data" : [
+        "menu.xml",
+        "service_team.xml",
+        "sale_order.xml",
+        "work_order.xml",
+        "report/workorder_report.xml",
+        "security/group.xml",
+        "security/ir.model.access.csv",
+    ],
+}
